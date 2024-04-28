c8ed92aa38aef5e6f19c7408300318f9b3d3a08d
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-04-29 03:44:03 +0500

    post_increment

---
+++ After_MID/Quiz_3 practice/increment.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class User
+{
+	private:
+	int x , y;
+	public:
+	User(int a=0 , int b=0)
+	{
+		x = a;
+		y = b;
+	}	
+	User operator ++ (int)
+	{
+            User temp = *this;
+		++x;
+		++y;
+            return temp;
+	} 
+	void display()
+	{
+		cout<<"Your value is "<< x <<endl;
+		cout<<"Your value of y "<< y <<endl;
+	}
+};
+int main()
+{
+      system("cls");
+	User u1(4 , 5);
+	u1.display();
+	++u1;
+	u1.display();
+	return 0;
+}
