c365a1683a9e8baf5eee775293d85eb692b719c0
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-05-05 01:37:08 +0500

    casting

---
+++ After_MID/Type_Casting/syntax.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int num1 ;
+      double num2 =9.71;
+      num1 = num2;
+      cout<<"Num 1 is : "<< num1 <<endl;
+      cout<<" Num 2 is : "<< num2 <<endl;
+}
